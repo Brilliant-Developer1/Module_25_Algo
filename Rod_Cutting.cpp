5daf3ec66f9fe652c812eb54fd01f50269b5b71b
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-09-27 23:26:24 +0600

    file edited

---
+++ Rod_Cutting.cpp
@@ -0,0 +1,107 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+int dp[1005][1005];
+
+int unbounded_Knapsack(int n, int s, int val[], int w[])
+{
+
+    if (n == 0 || s == 0)
+        return 0;
+    if (dp[n][s] != -1)
+        return dp[n][s];
+    if (w[n - 1] <= s)
+    {
+        int ch1 = val[n - 1] + unbounded_Knapsack(n, s - w[n - 1], val, w);
+        int ch2 = unbounded_Knapsack(n - 1, s, val, w);
+
+        return dp[n][s] = max(ch1, ch2);
+    }
+    else
+    {
+        return dp[n][s] = unbounded_Knapsack(n - 1, s, val, w);
+    }
+}
+int main()
+{
+
+    int n;
+    cin >> n;
+    int val[n], w[n];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val[i];
+        w[i] = i + 1;
+    }
+    cout << unbounded_Knapsack(n, n, val, w) << endl;
+
+    return 0;
+}

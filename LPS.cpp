19662fc1e0b8de63a63cafd0a895ec63f81e2dce
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-10-02 23:26:18 +0600

    Shortest_Common Added

--- LPS.cpp
+++ LPS.cpp
@@ -63,9 +63,11 @@ using namespace std;
 int main()
 {
 
-    string a, b;
-    cin >> a >> b;
+    string a;
+    cin >> a;
     int n = a.size();
+    string b = a;
+    reverse(b.begin(), b.end());
     int m = b.size();
 
     int dp[n + 1][m + 1];
@@ -78,7 +80,6 @@ int main()
                 dp[i][j] = 0;
         }
     }
-
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
@@ -104,29 +105,20 @@ int main()
             i--;
             j--;
         }
-        else if (dp[i - 1][j] > dp[i][j - 1])
-        {
-            ans += a[i - 1];
-            i--;
-        }
         else
         {
-            ans += b[j - 1];
-            j--;
+            if (dp[i][j - 1] > dp[i - 1][j])
+            {
+                j--;
+            }
+            else
+            {
+                i--;
+            }
         }
     }
-    while (i != 0)
-    {
-        ans += a[i - 1];
-        i--;
-    }
-    while (j != 0)
-    {
-        ans += b[j - 1];
-        j--;
-    }
-
     reverse(ans.begin(), ans.end());
+
     cout << ans << endl;
     return 0;
 }
---
+++ PSC.cpp
@@ -0,0 +1,99 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    string a, b;
+    cin >> a >> b;
+    int n = a.size();
+    int m = b.size();
+
+    int dp[n + 1][m + 1];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (a[i - 1] == b[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    cout << n + m - dp[n][m] << endl;
+    return 0;
+}
